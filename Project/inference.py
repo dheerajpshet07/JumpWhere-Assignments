ef873f3e048437a1c945985b3373c13ca6a2faeb
Dheeraj P Shet <83324158+dheerajpshet07@users.noreply.github.com>
2023-05-11 08:20:13 +0530

    Delete Project directory

--- Project/inference.py
+++
@@ -1,6 +0,0 @@
-import os
-from gensim.models.doc2vec import Doc2Vec
-
-def get_inference_vector (doc) :
-    model = Doc2Vec.load (os.path.join ("models", "doc2vec_20news.bin"))
-    return model.infer_vector (doc)
